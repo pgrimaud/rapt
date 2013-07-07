2ad904d915c18f29ddf003d1d1db8485a7f60833
Piotr <piotr@Piotrs-Mac.local>
2013-07-07 23:14:28 +0200

    rer Pins

--- MapViewController.m
+++ MapViewController.m
@@ -35,8 +35,36 @@
 {
     [super viewDidLoad];
     
-    //[MapRapt setUserTrackingMode:true];
-    //MapRapt.zoomEnabled = true;
+    //Map with pins :)
+    
+    [MapRapt setUserTrackingMode:true];
+    MapRapt.zoomEnabled = true;
+    
+    NSString *csvFilePath = [[NSBundle mainBundle] pathForResource:@"ratp" ofType:@"csv"];
+    NSString *dataStr = [NSString stringWithContentsOfFile:csvFilePath encoding:NSUTF8StringEncoding error:nil];
+    
+    NSArray* allLinedStrings = [dataStr componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]];
+    
+    for (NSString *line in allLinedStrings)
+    {
+        NSLog(@"%@", line);
+        
+        NSArray* infos = [line componentsSeparatedByString: @"#"];
+        if ([infos count] > 1)
+        {
+            NSString * latitude = [infos objectAtIndex:2];
+            NSString * longitude = [infos objectAtIndex:1];
+            NSString * crimeDescription =[infos objectAtIndex:3];
+            NSString * address = [infos objectAtIndex:4];
+            
+            CLLocationCoordinate2D coordinate;
+            coordinate.latitude = latitude.doubleValue;
+            coordinate.longitude = longitude.doubleValue;
+            Location *annotation = [[Location alloc] initWithName:crimeDescription address:address coordinate:coordinate] ;
+            [MapRapt addAnnotation:annotation];
+            
+        }
+    }
     _sidebarButton.target = self.revealViewController;
     _sidebarButton.action = @selector(revealToggle:);
     _sidebarButton.tintColor = [UIColor colorWithWhite:0.96f alpha:0.2f];
